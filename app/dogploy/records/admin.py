61cec4209ec4c81718a4cc1edd62ec5732021787
kjpark <kjedidiahpark@gmail.com>
2019-09-29 16:14:25 -0400

    admin worx

--- app/dogploy/records/admin.py
+++ app/dogploy/records/admin.py
@@ -6,9 +6,3 @@ from .models import Vet, Animal, Owner
 admin.site.register(Vet)
 admin.site.register(Animal)
 admin.site.register(Owner)
-
-class VetAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
-
-
-admin.site.register(Vet, VetAdmin)
