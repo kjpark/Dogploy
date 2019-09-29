552fe444cc99014e2182721332be0e3c1aabd3c4
kjpark <kjedidiahpark@gmail.com>
2019-09-29 16:41:45 -0400

    JUST COMMIT IT DUDE

--- app/dogploy/records/views.py
+++ app/dogploy/records/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Vet, Animal, Owner
+
+# def index(request):
+ #    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  #   template = loader.get_template('polls/index.html')
+  #   context = {
+  #       'latest_question_list': latest_question_list,
+  #   }
+#    return HttpResponse(template.render(context, request))
+
+def index(request):
+    vets = Vet.objects.all()
+    animals = Animal.objects.all()
+    owners = Owner.objects.all()
+    context = {
+        'vets': vets,
+        'animals': animals,
+        'owners': owners,
+    }
+    return render(request, 'records/index.html', context)
+
