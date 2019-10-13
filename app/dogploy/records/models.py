25c362f77d2c27b64a1dbbbd8da2104cb607379f
kjpark <kjedidiahpark@gmail.com>
2019-10-13 16:37:59 -0400

    create working tests

--- app/dogploy/records/models.py
+++ app/dogploy/records/models.py
@@ -3,12 +3,18 @@ from django.db import models
 class Vet(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    def introduce(self):
+        return "Hello, I am {} {}".format(self.first_name, self.last_name)
 
 class Animal(models.Model):
     species = models.CharField(max_length=30)
     age = models.IntegerField()
     owner = models.CharField(max_length=30)
+    def introduce(self):
+        return "I am a {}-year old {} owned by {}".format(self.age, self.species, self.owner)
 
 class Owner(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=30)
+    def introduce(self):
+        return "I am {} and I live at: {}".format(self.name, self.address)
--- app/dogploy/records/tests.py
+++ app/dogploy/records/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from records.models import Vet, Animal, Owner
 
-# Create your tests here.
+class VetTestCase(TestCase):
+    def setUp(self):
+        Vet.objects.create(first_name="Docker", last_name="Whalius")
+        Vet.objects.create(first_name="B0G7S", last_name="N0T4DOC")
+        
+    def test_vet_introduction(self):
+        """Vets can properly introduce themselves"""
+        dock = Vet.objects.get(first_name = "Docker")
+        B0G = Vet.objects.get(first_name = "B0G7S")
+        self.assertEqual(dock.introduce(), "Hello, I am Docker Whalius")
+        self.assertEqual(B0G.introduce(), "Hello, I am B0G7S N0T4DOC")
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Animal.objects.create(species="axolotl", age=1, owner="Kenobi")
+        Animal.objects.create(species="kangaroo", age=5, owner="Mars")
+
+    def test_animal_introduction(self):
+        axo = Animal.objects.get(species = "axolotl")
+        kang = Animal.objects.get(species = "kangaroo")
+        self.assertEqual(axo.introduce(), "I am a 1-year old axolotl owned by Kenobi")
+        self.assertEqual(kang.introduce(), "I am a 5-year old kangaroo owned by Mars")
+
+class OwnerTestCase(TestCase):
+    def setUp(self):
+        Owner.objects.create(name="Crozet Pizza", address="1111 Somewhere Elliewood")
+        Owner.objects.create(name="Tony Bennett (the singer)", address="321 Outside World")
+
+    def test_owner_introduction(self):
+        crozet = Owner.objects.get(name="Crozet Pizza")
+        tony = Owner.objects.get(name = "Tony Bennett (the singer)")
+        self.assertEqual(crozet.introduce(), "I am Crozet Pizza and I live at: 1111 Somewhere Elliewood")
+        self.assertEqual(tony.introduce(), "I am Tony Bennett (the singer) and I live at: 321 Outside World")
+
+#class AnimalTestCase(TestCase):
+#    def setUp(self):
+#        Animal.objects.create(name="lion", sound="roar")
+#        Animal.objects.create(name="cat", sound="meow")
+#
+#    def test_animals_can_speak(self):
+#        """Animals that can speak are correctly identified"""
+#        lion = Animal.objects.get(name="lion")
+#        cat = Animal.objects.get(name="cat")
+#        self.assertEqual(lion.speak(), 'The lion says "roar"')
+#        self.assertEqual(cat.speak(), 'The cat says "meow"')
